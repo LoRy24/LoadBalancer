dd199de557876fff30a2250107ebf3214a6da916
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-06-17 16:26:23 +0200

    Create the bungee plugin's entry class

---
+++ BungeecordPlugin/src/main/java/com/github/lory24/loadbalancer/bungee/Entry.java
@@ -0,0 +1,22 @@
+package com.github.lory24.loadbalancer.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+import java.io.IOException;
+
+public final class Entry extends Plugin {
+
+    @Override
+    public void onEnable() {
+        try {
+            LoadBalancerBungee.INSTANCE.enable(this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        LoadBalancerBungee.INSTANCE.disable();
+    }
+}
