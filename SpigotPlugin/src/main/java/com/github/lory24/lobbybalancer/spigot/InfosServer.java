d8de16e36c96c2f052b4f55271d95956be85118b
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-06-17 14:29:56 +0200

    Fix code errors and update code design

--- SpigotPlugin/src/main/java/com/github/lory24/lobbybalancer/spigot/InfosServer.java
+++ SpigotPlugin/src/main/java/com/github/lory24/lobbybalancer/spigot/InfosServer.java
@@ -1,9 +1,7 @@
 package com.github.lory24.lobbybalancer.spigot;
 
-import com.github.lory24.lobbybalancer.spigot.ConfigValues;
 import com.github.lory24.lobbybalancer.spigot.utils.ServerInfos;
 import com.google.gson.Gson;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -13,10 +11,12 @@ import java.net.Socket;
 
 public class InfosServer implements Runnable {
 
+    private ServerSocket serverSocket;
+
     @Override
     public void run() {
         try {
-            ServerSocket serverSocket = new ServerSocket();
+            this.serverSocket = new ServerSocket();
             serverSocket.bind(new InetSocketAddress((String) ConfigValues.SERVER_HOST.get(), (int) ConfigValues.SERVER_PORT.get()));
             this.listening(); // Start the listening state of the socket
             serverSocket.close();
@@ -27,20 +27,18 @@ public class InfosServer implements Runnable {
 
     private void listening() throws IOException {
         while (!Thread.interrupted()) {
-            Socket socket = new Socket();
-            new Thread(() -> processConnection(socket)).start();
-            socket.close();
-        }
-    }
-
-    private void processConnection(@NotNull Socket socket) {
-        try {
-            String serverInfosJson = new Gson().toJson(new ServerInfos().initialize());
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.writeUTF(serverInfosJson);
-            dataOutputStream.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            Socket socket = this.serverSocket.accept();
+            new Thread(() -> {
+                try {
+                    String serverInfosJson = new Gson().toJson(new ServerInfos().initialize());
+                    DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                    dataOutputStream.writeUTF(serverInfosJson);
+                    dataOutputStream.close();
+                    socket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
         }
     }
 }
