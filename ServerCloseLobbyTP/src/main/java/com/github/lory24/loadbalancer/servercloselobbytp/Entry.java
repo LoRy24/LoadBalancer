393e034c1eb38cac5d45aea26d21bd9e48a0eb9e
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-06-20 21:51:54 +0200

    Problems

--- ServerCloseLobbyTP/src/main/java/com/github/lory24/loadbalancer/servercloselobbytp/Entry.java
+++ ServerCloseLobbyTP/src/main/java/com/github/lory24/loadbalancer/servercloselobbytp/Entry.java
@@ -6,22 +6,29 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("unused")
 public final class Entry extends JavaPlugin {
 
+    @SuppressWarnings({"LoopConditionNotUpdatedInsideLoop", "StatementWithEmptyBody", "UnstableApiUsage"})
     @Override
     public void onEnable() {
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, "LoadBalancer");
         getLogger().info("Plugin enabled! Remember that if you disable this plugin all the players will be kicked from the server!");
+
+        // Kick all the players when the plugin isn't enabled
+        new Thread(() -> {
+            while (this.isEnabled());
+            for (Player player: Bukkit.getOnlinePlayers()) {
+                Bukkit.getLogger().info("A");
+                ByteArrayDataOutput byteArrayDataOutput = ByteStreams.newDataOutput();
+                byteArrayDataOutput.writeUTF("ConnectOnServerClose");
+                player.sendPluginMessage(this, "LoadBalancer", byteArrayDataOutput.toByteArray());
+            }
+        });
     }
 
-    @SuppressWarnings("UnstableApiUsage")
     @Override
     public void onDisable() {
-        for (Player player: Bukkit.getOnlinePlayers()) {
-            ByteArrayDataOutput byteArrayDataOutput = ByteStreams.newDataOutput();
-            byteArrayDataOutput.writeUTF("ConnectOnServerClose");
-            player.sendPluginMessage(this, "LoadBalancer", byteArrayDataOutput.toByteArray());
-        }
         getLogger().info("Plugin disabled!");
     }
 }
